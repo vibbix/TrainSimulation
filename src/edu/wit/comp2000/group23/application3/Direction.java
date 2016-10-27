4a670173ed0899d3f0665e7db7724a1907033a16
Mark Beznos <mark.vibbix@gmail.com>
2016-10-27 10:27:31 -0400

    Direction enum
    -Direction enum added
    -Began work on ITrainRoute

---
+++ src/edu/wit/comp2000/group23/application3/Direction.java
@@ -0,0 +1,15 @@
+package edu.wit.comp2000.group23.application3;
+
+/**
+ * The Direction things are going
+ */
+public enum Direction {
+    /**
+     * Going 'uptown'.
+     */
+    Inbound,
+    /**
+     * Going 'downtown'
+     */
+    Outbound
+}
